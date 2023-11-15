970e3c35bead9bbc9c96ca739e0a90f767e2043a
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2023-11-15 20:12:54 -0300

    new: added test files

--- src/models/collection/collection.controller.spec.ts
+++ src/models/collection/collection.controller.spec.ts
@@ -2,10 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaModule } from '../../common/database/prisma.module';
 import { CollectionController } from './collection.controller';
 import { CollectionService } from './collection.service';
+import { Request } from 'express';
 
 describe('CollectionController', () => {
   let controller: CollectionController;
   let service: CollectionService;
+  let mockRequest: Request;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,6 +16,7 @@ describe('CollectionController', () => {
       providers: [CollectionService],
     }).compile();
 
+    mockRequest = { user: { id: '1' } } as Request;
     controller = module.get<CollectionController>(CollectionController);
     service = module.get<CollectionService>(CollectionService);
   });
@@ -21,5 +24,35 @@ describe('CollectionController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of collections', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValueOnce([{}] as any[]);
+
+      const result = await controller.findAll(mockRequest);
+
+      expect(result.length).toEqual(1);
+      expect(service.findAll).toBeCalledTimes(1);
+      expect(service.findAll).toHaveBeenCalledWith(mockRequest.user.id);
+    });
+  });
+
+  describe('findTop', () => {
+    it('should return an array of collections by filter', async () => {
+      jest
+        .spyOn(service, 'findTopCollections')
+        .mockResolvedValueOnce([{}] as any[]);
+
+      mockRequest.params = { filter: 'ANIME' };
+      const result = await controller.findTop(mockRequest);
+
+      expect(result.length).toEqual(1);
+      expect(service.findTopCollections).toBeCalledTimes(1);
+      expect(service.findTopCollections).toHaveBeenCalledWith(
+        mockRequest.user.id,
+        'ANIME',
+      );
+    });
   });
 });
--- src/models/collection/collection.service.spec.ts
+++ src/models/collection/collection.service.spec.ts
@@ -1,9 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaModule } from '../../common/database';
+import { PrismaModule, PrismaService } from '../../common/database';
 import { CollectionService } from './collection.service';
 
 describe('CollectionService', () => {
   let service: CollectionService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +13,57 @@ describe('CollectionService', () => {
     }).compile();
 
     service = module.get<CollectionService>(CollectionService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(prisma).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a list of collections', async () => {
+      prisma.collection.findMany = jest
+        .fn()
+        .mockResolvedValueOnce([{ id: '1', name: 'Top' }] as any[]);
+
+      prisma.$queryRaw = jest.fn().mockResolvedValueOnce([] as any[]);
+
+      const response = await service.findAll('1');
+      expect(response.length).toEqual(1);
+      expect(prisma.collection.findMany).toBeCalledTimes(1);
+    });
+
+    it('should return a error', async () => {
+      prisma.collection.findMany = jest.fn().mockResolvedValueOnce([] as any[]);
+      prisma.$queryRaw = jest.fn().mockResolvedValueOnce([] as any[]);
+
+      expect(service.findAll('1')).rejects.toThrowError();
+      expect(prisma.collection.findMany).toBeCalledTimes(1);
+    });
+  });
+
+  describe('findTopCollections', () => {
+    it('should return a error', async () => {
+      prisma.collection.findMany = jest.fn().mockResolvedValueOnce([] as any[]);
+      prisma.$queryRaw = jest.fn().mockResolvedValueOnce([] as any[]);
+
+      expect(service.findTopCollections('1', 'NONE')).rejects.toThrowError();
+      expect(prisma.collection.findMany).toBeCalledTimes(0);
+    });
+
+    it('should return a list of collections by TOP filter', async () => {
+      prisma.collection.findMany = jest
+        .fn()
+        .mockResolvedValue([{ id: '1', name: 'Top' }] as any[]);
+      prisma.$queryRaw = jest.fn().mockResolvedValue([] as any[]);
+
+      const result = await service.findTopCollections('1', 'animes');
+      const result2 = await service.findTopCollections('1', 'playlist');
+
+      expect(result.length).toBe(1);
+      expect(result2.length).toBe(1);
+      expect(prisma.collection.findMany).toBeCalledTimes(2);
+    });
   });
 });
